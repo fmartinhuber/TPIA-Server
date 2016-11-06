4b5aeabcb7279f1a7ad737a7fa2d83cec8093c83
ramir <ramir@LAPTOP-3T82OQBM>
2016-11-06 14:52:07 -0300

    ArticuloEJB

--- TPA-Server/ejbModule/negocio/ArticuloEJB.java
+++ TPA-Server/ejbModule/negocio/ArticuloEJB.java
@@ -3,12 +3,12 @@ package negocio;
 import interfaz.ArticuloEJBLocal;
 import interfaz.ArticuloEJBRemote;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
 import bean.ArticuloBean;
@@ -21,26 +21,61 @@ import dto.ArticuloDTO;
 @LocalBean
 public class ArticuloEJB implements ArticuloEJBRemote, ArticuloEJBLocal {
 
+	@PersistenceContext
 	private EntityManager em;
 	
 	// Constructor
     
     public ArticuloEJB() {}
+    
+    // Metodos a implementar
 
+	
+	@SuppressWarnings("unchecked")
 	@Override
 	public List<ArticuloDTO> listarArticulos() {
-		// TODO Auto-generated method stub
-		Query q = em.createQuery("from ArticuloBean");
-		List<ArticuloBean> salida = new ArrayList<ArticuloBean>();
-		salida = q.getResultList();
-		System.out.println(salida.get(0));
-		return null;				
+
+		Query q = em.createQuery("Select a FROM ArticuloBean a");
+		List<ArticuloDTO> salida = new ArrayList<ArticuloDTO>();
+		List<ArticuloBean> articulos = new ArrayList<ArticuloBean>();
+		articulos = q.getResultList();
+		
+		for(ArticuloBean a : articulos){
+			salida.add(a.aArticuloDTO());
+		}		
+		return salida;					
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<ArticuloBean> listarArticulos2() {
+		return em.createQuery("SELECT a FROM ArticuloBean a").getResultList();
 	}
 
 	@Override
-	public void crearArticulo(ArticuloDTO articulo) {
-		// TODO Auto-generated method stub
-		
+	public void persist(ArticuloDTO articulo) {		
+		em.persist(articulo);
 	}
+
+	@Override
+	public void remove(ArticuloDTO articulo) {
+		em.remove(articulo);
+	}
+
+	@Override
+	public void update(ArticuloDTO articulo) {
+		em.merge(articulo);
+	}
+
+	@Override
+	public ArticuloDTO buscarArticuloPorCodigo(Integer codArticulo) {
+		
+		return (ArticuloDTO) em.createQuery("SELECT a FROM ArticuloBean a where a.codArticulo= :codArticulo").setParameter("codArticulo", codArticulo).getSingleResult();		
+	}
+
+	@Override
+	public ArticuloDTO buscarArticuloPorNombre(String nombre) {
+		
+		return (ArticuloDTO) em.createQuery("SELECT a FROM ArticuloBean a where a.nombre = :nombre").setParameter("nombre", nombre).getSingleResult();			
+	}		
 
 }
--- TPA-Server/ejbModule/test/TestRama.java
+++ TPA-Server/ejbModule/test/TestRama.java
@@ -1,23 +1,38 @@
 package test;
 
-import java.util.ArrayList;
+import java.util.*;
 
 import bean.*;
 import controler.*;
 import dao.*;
 import dto.*;
+import interfaz.ArticuloEJBLocal;
+import negocio.*;
 
 public class TestRama {
 
 	public static void main(String[] args) {
 		
-		ArrayList<ArticuloDTO> articulos = new ArrayList<ArticuloDTO>();
-		articulos = (ArrayList<ArticuloDTO>) DepositoControlador.getInstancia().listarArticulos();
+//		List<ArticuloDTO> articulosDTO = new ArrayList<ArticuloDTO>();
+//						
+//		ArticuloEJBLocal articuloEJB = new ArticuloEJB();
+//		articulosDTO = articuloEJB.listarArticulos();
+//		
+//		for(ArticuloDTO a : articulosDTO){
+//			System.out.println("Codigo: " +a.getCodArticulo());
+//		}
 		
-		for(ArticuloDTO a : articulos){
-		System.out.println("Codigo: " +a.getCodArticulo());
-		}
 		
+		ArticuloEJBLocal articuloEJB2 = new ArticuloEJB();
+		ArticuloDTO newArticulo = new ArticuloDTO();
+		
+		newArticulo.setCodArticulo(666);
+		
+		articuloEJB2.persist(newArticulo);
+		
+//		List<ArticuloBean> articulosBean = new ArrayList<ArticuloBean>();
+//		ArticuloEJBLocal articuloEJB = new ArticuloEJB();
+//		articulosBean = articuloEJB.listarArticulos2();
 		
 //		ArrayList<ArticuloBean> articulos = (ArrayList<ArticuloBean>) DepositoControlador.getInstancia().listarArticulos();
 //		
